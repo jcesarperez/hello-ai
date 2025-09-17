e37bde7f1c9c39b8697e9a6bb6ba45900be0a771
jcesarperez <julio.cesar.perez.arques@gmail.com>
2025-09-17 13:27:17 +0200

    feat: OpenAI example for streaming

---
+++ src/hello_openai_with_streaming.mjs
@@ -0,0 +1,41 @@
+import 'dotenv/config';
+import OpenAI from "openai";
+import { zodTextFormat } from "openai/helpers/zod";
+import { z } from "zod";
+
+const client = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+const EntitiesSchema = z.object({
+  attributes: z.array(z.string()),
+  colors: z.array(z.string()),
+  animals: z.array(z.string()),
+});
+
+const stream = client.responses
+  .stream({
+    model: "gpt-4.1",
+    input: [
+      { role: "user", content: "What's the weather like in Paris today?" },
+    ],
+    text: {
+      format: zodTextFormat(EntitiesSchema, "entities"),
+    },
+  })
+  .on("response.refusal.delta", (event) => {
+    process.stdout.write(event.delta);
+  })
+  .on("response.output_text.delta", (event) => {
+    process.stdout.write(event.delta);
+  })
+  .on("response.output_text.done", () => {
+    process.stdout.write("\n");
+  })
+  .on("response.error", (event) => {
+    console.error(event.error);
+  });
+
+const result = await stream.finalResponse();
+
+console.log(result);
